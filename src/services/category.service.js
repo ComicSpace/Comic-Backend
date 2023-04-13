671bbdaa1880c3fcf8d459b8bb93c6172ec1e376
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-04-13 03:16:58 +0100

    Create new category service

---
+++ src/services/category.service.js
@@ -0,0 +1,13 @@
+const Category = require('../models/category.model');
+
+class CategoryService {
+    async createNewCategory(data) {
+        const category = await Category.create(data);
+
+        await category.save();
+
+        return category;
+    }
+}
+
+module.exports = CategoryService;
