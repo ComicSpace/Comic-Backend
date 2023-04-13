d3af193c95511130323df32815fadc07a5780822
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-04-13 03:06:18 +0100

    Modify book schema

--- src/schemas/book.schemas.js
+++ src/schemas/book.schemas.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 exports.CreateBookSchema = Joi.object({
     title: Joi.string().min(3).max(100).trim().lowercase().required(),
-    description: Joi.string().min(3).max(100).trim().lowercase().required(),
+    description: Joi.string().min(3).max(1000).trim().lowercase().required(),
     author: Joi.string().min(3).max(100).trim().lowercase().required(),
     coverImageUrl: Joi.string().trim().lowercase().required(),
     pages: Joi.string().trim().lowercase().required(),
-    category: Joi.string().trim().lowercase().required(),
+    category: Joi.string().hex().trim().lowercase().required(),
 })
