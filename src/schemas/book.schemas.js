e6e270969442754471ccd9051fb7c91910b33707
Chinyere Akanno <adanmachinyere98@gmail.com>
2023-04-13 02:28:14 +0100

    Create Book Schema

---
+++ src/schemas/book.schemas.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+exports.CreateBookSchema = Joi.object({
+    title: Joi.string().min(3).max(100).trim().lowercase().required(),
+    description: Joi.string().min(3).max(100).trim().lowercase().required(),
+    author: Joi.string().min(3).max(100).trim().lowercase().required(),
+    coverImageUrl: Joi.string().trim().lowercase().required(),
+    pages: Joi.string().trim().lowercase().required(),
+    category: Joi.string().trim().lowercase().required(),
+})
